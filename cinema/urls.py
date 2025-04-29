7a24c40c313e7e15e2cfed38ed9212c77825a4a1
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-04-30 01:10:16 +0200

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from cinema.models import Order
+from cinema.serializers import TicketListSerializer
 from cinema.views import (
     GenreViewSet,
     ActorViewSet,
     CinemaHallViewSet,
     MovieViewSet,
     MovieSessionViewSet,
+    OrderViewSet,
+    TicketViewSet,
 )
 
 router = routers.DefaultRouter()
@@ -15,6 +19,8 @@ router.register("actors", ActorViewSet)
 router.register("cinema_halls", CinemaHallViewSet)
 router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
+router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
